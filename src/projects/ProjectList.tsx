6432e3d35cf788ad5fbe76ab0201e89a1933ed82
ramon <reimon1972@gmail.com>
2023-05-07 13:07:22 +0800

    first commit

---
+++ src/projects/ProjectList.tsx
@@ -0,0 +1,23 @@
+
+import { Project } from './Project';
+import ProjectCard from './ProjectCard'
+
+interface ProjectListProps {
+  projects: Project[];
+}
+
+function ProjectList({ projects }: ProjectListProps) {
+
+  return (
+    <div className="row">
+      {projects.map((project) => (
+        <div key={project.id} className="cols-sm">
+          <ProjectCard project={project} />
+        </div>
+      ))}
+    </div>
+  );
+
+}
+
+export default ProjectList;
